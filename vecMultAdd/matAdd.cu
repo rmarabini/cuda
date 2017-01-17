431a169703fe943a061cddb24227e594e1ac756d
roberto <roberto.marabini.uam.es>
2017-01-17 12:03:10 +0100

    w

--- vecMultAdd/matAdd.cu
+++ vecMultAdd/matAdd.cu
@@ -23,6 +23,7 @@ __global__ void Mat_add_Vector(float matIn[], float vRef[], float matOut[], int 
     int threadRow = blockIdx.x ;
     //if (blockIdx.y==0 && blockIdx.x==0)
        printf("col=%d, row=%d",threadCol,threadRow);
+    int indexOfMatrix = threadCol + threadRow * vecDim;
 
     if(threadCol < vecDim )
         {
