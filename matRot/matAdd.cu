c15eba63409c8411e6b721363c2ed56aa521c6ff
roberto <roberto.marabini.uam.es>
2017-01-17 23:02:53 +0100

    w

--- matRot/matAdd.cu
+++ matRot/matAdd.cu
@@ -217,8 +217,8 @@ int main(int argc, char* argv[]) {
    printf("Finished addition on GPU. Time taken: %5.5f\n", timeDifferenceOnDevice);   
    printf("Speedup: %5.5f\n", (float)timeDifferenceOnHost/timeDifferenceOnDevice);
 
-   Print_matrix("The rotated image(CPU) is: ", h_B2, dimX, dimY, 4, 5);
-   Print_matrix("The rotated image(GPU) is: ", h_B, dimX, dimY, 4, 5);
+   Print_matrix("The rotated image(CPU) is: ", h_B2, dimX, dimY, 9, 9);
+   Print_matrix("The rotated image(GPU) is: ", h_B, dimX, dimY, 9, 9);
 
    /* Free device memory */
    cudaFree(d_A);
