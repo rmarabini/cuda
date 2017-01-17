8a8664441910ac23b8d77088a2769eb38197410b
roberto <roberto.marabini.uam.es>
2017-01-17 12:27:16 +0100

    w

--- vecMultAdd/matAdd.cu
+++ vecMultAdd/matAdd.cu
@@ -157,10 +157,12 @@ int main(int argc, char* argv[]) {
    cudaEventRecord(deviceStart, 0);
    //d_A -> inMatrix, d_B vRef, d_C outMat
    Mat_add_Vector<<<block, grid>>>(d_A, d_B, d_C, numVec, dimVec);
-   cudaDeviceSynchronize();  
    int code=cudaGetLastError();
    if (code)
        printf("error=%s",cudaGetErrorString(code));
+   else
+       printf("code=%d",code);
+   cudaDeviceSynchronize();  
    cudaEventRecord(deviceStop, 0);
 
    /* Wait for the kernel to complete */
