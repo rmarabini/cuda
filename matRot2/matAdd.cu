598c9e50d1e89fc08ddf1b6a8ba88d1a0a38d4b3
roberto <roberto.marabini.uam.es>
2017-01-18 13:01:47 +0100

    bilineal interpolation CPU

--- matRot2/matAdd.cu
+++ matRot2/matAdd.cu
@@ -204,8 +204,8 @@ void rotateCPU(float matIn[],
 }
 /* Host code */
 int main(int argc, char* argv[]) {
-   size_t dimX = 21;//mat size
-   size_t dimY = 21;
+   size_t dimX = 31;//mat size
+   size_t dimY = 31;
    size_t gridX = 9;//mat size
    size_t gridY = 9;
 
@@ -301,8 +301,8 @@ int main(int argc, char* argv[]) {
    printf("Finished addition on GPU. Time taken: %5.5f\n", timeDifferenceOnDevice);   
    printf("Speedup: %5.5f\n", (float)timeDifferenceOnHost/timeDifferenceOnDevice);
 
-   Print_matrix("The rotated image(CPU) is: ", h_B2, dimX, dimY, 9, 20);
-   Print_matrix("The rotated image(GPU) is: ", h_B, dimX, dimY, 9, 20);
+   Print_matrix("The rotated image(CPU) is: ", h_B2, dimX, dimY, 9, 27);
+   Print_matrix("The rotated image(GPU) is: ", h_B, dimX, dimY, 9, 27);
 
    /* Free device memory */
    cudaFree(d_A);
